b778da5ecd1342666c9944d6456febe250f37111
albintomy0 <119939573+albintomy0@users.noreply.github.com>
2024-10-02 18:30:12 +1300

    hi ekansh

--- GUIgit/src/guigit/GUIgit.java
+++ GUIgit/src/guigit/GUIgit.java
@@ -15,6 +15,9 @@ public class GUIgit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hii");
+                System.out.println("EKANSH");
+
     }
     
 }
