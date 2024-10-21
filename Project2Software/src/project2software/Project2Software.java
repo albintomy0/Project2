f712c5bc1024849e03f6cd38a3830109927b974e
albintomy0 <119939573+albintomy0@users.noreply.github.com>
2024-10-21 13:05:22 +1300

    First output

--- Project2Software/src/project2software/Project2Software.java
+++ Project2Software/src/project2software/Project2Software.java
@@ -15,6 +15,8 @@ public class Project2Software {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("First message");
     }
     
 }
