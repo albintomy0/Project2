2d6d566be4b5d65bed54732a96709d33359ba852
ekans <ekanshk18@gmail.com>
2024-10-25 19:15:53 +1300

    test class1

--- Project2Software/test/Project2GUIPackage/ReceiptModelTest.java
+++ Project2Software/test/Project2GUIPackage/ReceiptModelTest.java
@@ -1,51 +1,141 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
 package Project2GUIPackage;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
 
+/**
+ *
+ * @author ekans
+ */
 public class ReceiptModelTest {
-
+    
+    public ReceiptModelTest() {
+    }
+    
     private ReceiptModel receiptModel;
 
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
     @BeforeEach
     public void setup() {
-        // Initialize the receipt model before each test
         receiptModel = new ReceiptModel("Hamilton", "2024-12-01", "11:00", "T006", 2, 1, 60.0);
     }
+    
+    @AfterEach
+    public void tearDown() {
+    }
 
+    /**
+     * Test of getDestination method, of class ReceiptModel.
+     */
     @Test
     public void testGetDestination() {
-        assertEquals("Hamilton", receiptModel.getDestination(), "Destination should be Hamilton");
+        System.out.println("getDestination");
+        ReceiptModel instance = null;
+        String expResult = "";
+        String result = instance.getDestination();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
 
+    /**
+     * Test of getDepartureDate method, of class ReceiptModel.
+     */
     @Test
     public void testGetDepartureDate() {
-        assertEquals("2024-12-01", receiptModel.getDepartureDate(), "Departure date should be 2024-12-01");
+        System.out.println("getDepartureDate");
+        ReceiptModel instance = null;
+        String expResult = "";
+        String result = instance.getDepartureDate();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
 
+    /**
+     * Test of getDepartureTime method, of class ReceiptModel.
+     */
     @Test
     public void testGetDepartureTime() {
-        assertEquals("11:00", receiptModel.getDepartureTime(), "Departure time should be 11:00");
+        System.out.println("getDepartureTime");
+        ReceiptModel instance = null;
+        String expResult = "";
+        String result = instance.getDepartureTime();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
 
+    /**
+     * Test of getTrainID method, of class ReceiptModel.
+     */
     @Test
     public void testGetTrainID() {
-        assertEquals("T006", receiptModel.getTrainID(), "Train ID should be T006");
+        System.out.println("getTrainID");
+        ReceiptModel instance = null;
+        String expResult = "";
+        String result = instance.getTrainID();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
 
+    /**
+     * Test of getAdultTickets method, of class ReceiptModel.
+     */
     @Test
     public void testGetAdultTickets() {
-        assertEquals(2, receiptModel.getAdultTickets(), "There should be 2 adult tickets");
+        System.out.println("getAdultTickets");
+        ReceiptModel instance = null;
+        int expResult = 0;
+        int result = instance.getAdultTickets();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
 
+    /**
+     * Test of getChildTickets method, of class ReceiptModel.
+     */
     @Test
     public void testGetChildTickets() {
-        assertEquals(1, receiptModel.getChildTickets(), "There should be 1 child ticket");
+        System.out.println("getChildTickets");
+        ReceiptModel instance = null;
+        int expResult = 0;
+        int result = instance.getChildTickets();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
 
+    /**
+     * Test of getTotalCost method, of class ReceiptModel.
+     */
     @Test
     public void testGetTotalCost() {
-        assertEquals(60.0, receiptModel.getTotalCost(), 0.01, "Total cost should be 60.0");
+        System.out.println("getTotalCost");
+        ReceiptModel instance = null;
+        double expResult = 0.0;
+        double result = instance.getTotalCost();
+        assertEquals(expResult, result, 0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
+    
 }
